7262bb10d9fa15e127deb65adb1ad900771d88e6
Jordan anderson <mr@jordananderson.io>
2021-11-04 14:26:01 +0000

    Add models

---
+++ Oakbrook.CoffeeShop/Models/Checkout.cs
@@ -0,0 +1,19 @@
+﻿namespace Oakbrook.CoffeeShop.Controllers
+{
+  public class Checkout
+  {
+    public int HouseNumber { get; set; }
+    public string StreetAddress { get; set; }
+    public string PhoneNumber { get; set; }
+    public string PostCode { get; set; }
+    public string CardNumber { get; set; }
+    public string ExpiryDate { get; set; }
+    public int CVV { get; set; }
+    public string DiscountCode { get; set; }
+    public decimal Discount { get; set; }
+    public int BasketId { get; set; }
+    public int UserId { get; set; }
+    public int? AddressId { get; set; }
+    public int? CardId { get; set; }
+  }
+}
